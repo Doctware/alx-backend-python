8f27a9476656cbfc2a5501b79f33d7e866afaa9a
Doctware <yusclever9@gmail.com>
2024-10-11 23:10:24 +0100

    str representation of flot

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+""" this module contains a type-anotation function that
+    takes float as args and returns str of the float """
+
+
+def to_str(n: float) -> str:
+    """
+    this function takes float as args ans returns str
+    repsentation if float"""
+
+    return str(n)
