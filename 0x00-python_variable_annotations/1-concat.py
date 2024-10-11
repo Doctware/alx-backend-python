31caaf87a0eaef178a57456973e638e09d0516d7
Doctware <yusclever9@gmail.com>
2024-10-11 21:40:50 +0100

    concatenate str

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" this module contains type-annotation function that takes
+    str an args and returns concatenated str """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    this function takes two str args and returns
+    concatenated str
+    """
+    return str1 + str2
