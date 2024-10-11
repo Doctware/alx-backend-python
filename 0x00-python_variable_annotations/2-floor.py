88ce2677da473ae27fcc1323432978cccfe552be
Doctware <yusclever9@gmail.com>
2024-10-11 22:16:25 +0100

    floating point

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" this module contains a type annotation
+    thats take float as an args the returns float """
+
+
+def floor(n: float) -> float:
+    """
+    this function takes a float as args and returns a flost value """
+    return n
