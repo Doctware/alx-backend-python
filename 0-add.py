b17e07fc6d7111c3c131d32e09ee97611ee25d03
Doctware <yusclever9@gmail.com>
2024-10-11 12:20:19 +0100

    Basic annotation add

---
+++ 0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" this module a type-anotatated function thats takes
+float as args then return float """
+
+
+def add(a: float, b: float) -> float:
+    """
+    An type-anotated function that takes float as args
+    and return float value
+    """
+    return a + b
