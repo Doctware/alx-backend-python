3a73f1b441286b34ba6287ed492ba43ce4d5df81
Doctware <yusclever9@gmail.com>
2024-10-15 01:54:20 +0100

    cocurrent corroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ this module implement cocurrent corouties """
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -7,8 +8,10 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     """
     this function implement cocurrent coroutines
     """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+
     delays = []
-    for _ in range(n):
-        delay = await wait_random(max_delay)
+    for task in asyncio.as_completed(tasks):
+        delay = await task
         delays.append(delay)
     return delays
