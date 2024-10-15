1432193447c648bd9b1944f13727c7edd0ca562e
Doctware <yusclever9@gmail.com>
2024-10-15 23:32:11 +0100

    coroutines generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" this module contains coroutines i.e a generator """
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    this coroutines loops 10 times
+    each time asyncronous wait 1sec the yield a random number
+    bitween 0 - 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
