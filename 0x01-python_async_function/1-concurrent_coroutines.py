b528cfa7c4cc407a12db6f996060aa74828f782b
Doctware <yusclever9@gmail.com>
2024-10-15 11:30:11 +0100

    concurrent coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """ this module implement cocurrent coroutines
     impoerting basic syntac from basic async """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     this function implement cocurrent coroutines
     """
