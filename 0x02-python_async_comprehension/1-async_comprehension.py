859632711f94057ef736a86c2f0770cd7a70b549
Doctware <yusclever9@gmail.com>
2024-10-16 00:21:03 +0100

    Async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" this module contains a corutines thats implement comprehesion """
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehesion() -> List[float]:
+    """
+    this corutines collecnt random number using an async comprehesion
+    over async_generator then return the 10 random nuber
+    """
+    random = [i async for i in async_generator()]
+    return random
